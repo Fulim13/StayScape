10b0d7f9e2d5eaf224ec006678eff3042fd35b9f
Fu Lim <fulim1130@gmail.com>
2024-03-22 23:32:39 +0800

    Add Report Page

--- StayScape/StayScape/Main.Master.designer.cs
+++ StayScape/StayScape/Main.Master.designer.cs
@@ -69,6 +69,15 @@ namespace StayScape
         protected global::System.Web.UI.WebControls.HyperLink reviewLink;
 
         /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+
+        /// <summary>
         /// SiteMapPath1 control.
         /// </summary>
         /// <remarks>
