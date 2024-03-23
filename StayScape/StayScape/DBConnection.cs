a2efb9b440511989e861291762d2cd09862871b5
Fu Lim <fulim1130@gmail.com>
2024-03-24 01:00:40 +0800

    Create Connection

---
+++ StayScape/StayScape/DBConnection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace StayScape
+{
+    public class DBConnection
+    {
+        private string connectionString =
+            ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection connection;
+
+
+        public SqlCommand ExecuteQuery(string query, SqlParameter[] parameters = null)
+        {
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            return command;
+
+
+        }
+
+        public Boolean ExecuteNonQuery(string nonQuery, SqlParameter[] parameters = null)
+        {
+
+            SqlCommand command = new SqlCommand(nonQuery, connection);
+
+            if (parameters != null)
+            {
+                command.Parameters.AddRange(parameters);
+            }
+
+            return command.ExecuteNonQuery() > 0 ? true : false;
+        }
+
+        public void createConnection()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+    }
+}
