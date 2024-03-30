811c6eb19f7cf402f5facb6d5a82be6a53f27de4
DesmondTZX <desmondtzx@outlook.com>
2024-03-31 00:27:31 +0800

    login is still being debugged, redirect to profile is working, need to deal with host part

--- StayScape/StayScape/Login.aspx.cs
+++ StayScape/StayScape/Login.aspx.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Text;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace StayScape.DesmondsPage
 {
@@ -9,27 +15,88 @@ namespace StayScape.DesmondsPage
     {
         protected void BtnSignIn_Click(object sender, EventArgs e)
         {
-            string emailNum = txtMailNum.Text;
+            string input = txtMailNum.Text.Trim();
             string password = txtPassword.Text;
+            string hashedInput = Convert.ToBase64String(System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(password)));
 
-            bool isValid = ValidateUser(emailNum, password);
-
-            if (isValid)
+            using (SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\desmo\Documents\Web_Dev_Assignment\StayScape\StayScape\App_Data\StayScapeDB.mdf;Integrated Security=True"))
             {
-                Session["LoggedIn"] = true;
+                con.Open();
 
-                Response.Redirect("Profile.aspx");
-            }
-            else
-            {
-                // logged in failed
+                string query = "SELECT 'Customer' AS userType, custEmail, custPhoneNumber FROM Customer WHERE (custEmail = @login OR custPhoneNumber = @login) AND custPassword = @password";
+                string queryHost = "SELECT 'Host' AS userType, hostEmail, hostPhoneNumber FROM Host WHERE (hostEmail = @login OR hostPhoneNumber = @login) AND hostPassword = @password";
+
+
+                using (SqlCommand cmd = new SqlCommand(query + " UNION ALL " + queryHost, con))
+                {
+                    cmd.Parameters.AddWithValue("@login", input);
+                    cmd.Parameters.AddWithValue("@password", hashedInput);
+
+                    // Debug.WriteLine("Query: " + cmd.CommandText + "; @login = " + input + "; @password = " + hashedInput);
+
+                    using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                    {
+                        DataTable dt = new DataTable();
+                        sda.Fill(dt);
+
+                        bool redirectToProfile = false;
+                        bool redirectToHostCheck = false;
+
+                        if (dt.Rows.Count > 0)
+                        {
+                            string userType = dt.Rows[0]["userType"].ToString();
+
+                            if (userType == "Customer")
+                            {
+                                Debug.WriteLine("User is in Customer role.");
+                                // Store phone number in session for later use
+                                Session["phoneNumber"] = dt.Rows[0]["custPhoneNumber"].ToString();
+                                redirectToProfile = true;
+                            }
+                            else if (userType == "Host")
+                            {
+                                Debug.WriteLine("User is in Host role.");
+                                // Store phone number in session for later use
+                                // Session["phoneNumber"] = dt.Rows[0]["hostPhoneNumber"].ToString();
+                                foreach (DataRow row in dt.Rows)
+                                {
+                                    Console.WriteLine("Host Email: " + row["hostEmail"]);
+                                    Console.WriteLine("Host Phone Number: " + row["hostPhoneNumber"]);
+                                }
+                                redirectToHostCheck = true;
+                            }
+                            else
+                            {
+                                Debug.WriteLine("User is not in any role.");
+                                // Handle the case where the user is not in any role
+                                Response.Write("User is not in any role.");
+                            }
+                        }
+                        else
+                        {
+                            Debug.WriteLine("No rows returned from query.");
+                            // Handle the case where no rows were returned from the query
+                            Response.Write("Invalid credentials.");
+                        }
+
+                        // Redirect based on the role
+                        if (redirectToProfile)
+                        {
+                            Debug.WriteLine("Redirecting to Profile.aspx");
+                            Response.Redirect("Profile.aspx");
+                        }
+                        else if (redirectToHostCheck)
+                        {
+                            Debug.WriteLine("Redirecting to HostCheck.aspx");
+                            Response.Redirect("HostCheck.aspx");
+                        }
+
+
+
+                    }
+                }
             }
         }
 
-        private bool ValidateUser(string emailNum, string password)
-        {
-            //logic here, otherwise, always true
-            return true;
-        }
     }
 }
--- StayScape/StayScape/Profile.aspx.cs
+++ StayScape/StayScape/Profile.aspx.cs
@@ -11,7 +11,7 @@ namespace StayScape
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
